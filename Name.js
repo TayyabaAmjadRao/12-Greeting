f5e1178b95a9585e61ce091ce841c059c5993b74
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 12:42:16 -0800

    Add files via upload

---
+++ Name.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['TAYYABA', 'AMJAD', 'RAO', 'ZADI'];
+const message = 'Welcome to assignment task:';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ Name.ts
@@ -0,0 +1,9 @@
+let names :string[]=['TAYYABA','AMJAD','RAO','ZADI'];
+
+const message :string = 'Welcome to assignment task:' 
+
+for(let i=0; i<names.length; i++){
+
+console.log(message + names[i]);
+
+}
